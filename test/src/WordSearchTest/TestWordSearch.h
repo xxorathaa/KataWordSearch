5c9c2696c236d200c04d7a9a58330c70b1ba89f9
xxorathaa <xxorathaa@gmail.com>
2018-11-13 17:31:14 -0600

    file reorganization, includes folder ended up irrelevant

---
+++ test/src/WordSearchTest/TestWordSearch.h
@@ -0,0 +1,36 @@
+#pragma once
+
+#include <cppunit/extensions/HelperMacros.h>
+
+#include <WordSearch/WordSearch.h>
+
+class TestWordSearch : public CppUnit::TestFixture
+{
+   CPPUNIT_TEST_SUITE( TestWordSearch );
+   CPPUNIT_TEST( testReadingMissingFile );
+   CPPUNIT_TEST( testReadingExistingFile );
+   CPPUNIT_TEST( testGetSearchWord );
+   CPPUNIT_TEST( testSetSearchWords );
+   CPPUNIT_TEST( testSetPuzzleRow );
+   CPPUNIT_TEST( testSearch_firstWord );
+   CPPUNIT_TEST( testSearch_secondWord );
+   CPPUNIT_TEST( testFindWord );
+   CPPUNIT_TEST( testGetSearchWordQuantity );
+   CPPUNIT_TEST_SUITE_END();
+
+public:
+   void setUp();
+   void tearDown();
+   void testReadingMissingFile();
+   void testReadingExistingFile();
+   void testGetSearchWord();
+   void testSetSearchWords();
+   void testSetPuzzleRow();
+   void testSearch_firstWord();
+   void testSearch_secondWord();
+   void testFindWord();
+   void testGetSearchWordQuantity();
+
+private:
+   WordSearch* _ws;
+};
